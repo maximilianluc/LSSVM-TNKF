30e5d77670d0124c27061963e692af09981d47ac
MaxiL-projects <74817215+MaxiL-projects@users.noreply.github.com>
2021-02-13 11:16:51 +0100

    Update RBFKernelMax.m

--- Two-Spiral/Function & Classes/RBFKernelMax.m
+++ Two-Spiral/Function & Classes/RBFKernelMax.m
@@ -1,7 +1,6 @@
 function [RBF_row] = RBFKernelMax(Data,gamma,sig2,n,d,k)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
-%Data2 = Data';
 
 RBF_row = zeros(1,n^d);
 diff = zeros(n^d,size(Data,2));
@@ -9,18 +8,8 @@ i = k;
 diff(1:n^d,:) = Data(i,:)-Data;
 
 
-%diff(1:n^d,:) = bsxfun(@minus, Data(i,:), Data); same as above
-%for j = 1:1:n^d %j is the time 
-    %diff(:,j) =    (Data(i,:)-Data(j,:))';  %instead of Data(:,i)-Data(:,j);
-    %RBF_row(1,j) = exp( -(sum((diff(j,:).*diff(j,:))./(2*sig2))) ); %werkt =)
-    
-%end
-
 RBF_row = (exp( -((sum(diff.*diff,2))'./(2*sig2))) );
-% for j = 1:1:n^d
-%     %diff(:,j) =    (Data(i,:)-Data(j,:))';  %instead of Data(:,i)-Data(:,j);
-%     RBF_row(1,j) = exp( -(sum((diff(j,:).*diff(j,:))./(2*sig2))) ); %werkt =)
-% end
+
 
 RBF_row(1,i) = RBF_row(1,i) + 1/gamma;
 end
