c98a08849cfc726a7a98daf5c7e17b4e692ca809
MaxiL-projects <74817215+MaxiL-projects@users.noreply.github.com>
2021-02-13 11:16:25 +0100

    Update LinearKernelMax.m

--- Two-Spiral/Function & Classes/LinearKernelMax.m
+++ Two-Spiral/Function & Classes/LinearKernelMax.m
@@ -8,19 +8,7 @@ diff = zeros(n^d,size(Data,2));
 i = k;
 diff(1:n^d,:) = Data(i,:)-Data;
 
-
-%diff(1:n^d,:) = bsxfun(@minus, Data(i,:), Data); same as above
-%for j = 1:1:n^d %j is the time 
-    %diff(:,j) =    (Data(i,:)-Data(j,:))';  %instead of Data(:,i)-Data(:,j);
-    %RBF_row(1,j) = exp( -(sum((diff(j,:).*diff(j,:))./(2*sig2))) ); %werkt =)
-    
-%end
-
 RBF_row =  sum(diff.*diff,2)' ;
-% for j = 1:1:n^d
-%     %diff(:,j) =    (Data(i,:)-Data(j,:))';  %instead of Data(:,i)-Data(:,j);
-%     RBF_row(1,j) = exp( -(sum((diff(j,:).*diff(j,:))./(2*sig2))) ); %werkt =)
-% end
 
 RBF_row(1,i) = RBF_row(1,i) + 1/gamma;
 end
