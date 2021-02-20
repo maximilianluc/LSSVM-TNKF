a24047a733fb2a8442ad2b7e149e698b8fe58ba9
MaxiL-projects <74817215+MaxiL-projects@users.noreply.github.com>
2021-02-20 18:17:47 +0100

    Update Nystrom_Adult.m

--- Adult/Nystrom_Adult.m
+++ Adult/Nystrom_Adult.m
@@ -28,8 +28,8 @@ d_t = 7;
 X_p = X_unsorted(1:n_p^d_p,:);
 Labels_p = Labels_unsorted(1:n_p^d_p,:);
 %%% Sort training data
-[Labels_p,I] = sort(Labels_p,'descend');
-X_p = X_p(I,:);
+%[Labels_p,I] = sort(Labels_p,'descend');
+%X_p = X_p(I,:);
 
 
 X_t = X_unsorted((n_p^d_p)+1:(n_p^d_p)+(n_t^d_t)+1,:); 
@@ -40,48 +40,25 @@ Labels_t = Labels_unsorted((n_p^d_p)+1:(n_p^d_p)+(n_t^d_t)+1);
 
 gam  = 0.0015; %   best gamma around 0.00005 - 0.0005                                             
 sig2 = 0.5;  %best sigma around 0.5
-nb = 100;
+nb = 50;
 
+S = 3^6;
+RandPermutation = randperm(length(X_p))
+Subset = X_p(RandPermutation(1:S),:)
+Subset_labels = Labels_p(RandPermutation(1:S),:)
 
 tic
-[V, D] = eign(X_p, 'RBF_kernel', sig2, nb);
+[V, D] = eign(Subset, 'RBF_kernel', sig2, nb);
+diagD = diag(D);
+alpha = gam*(Subset_labels - (V*inv((1/gam)*eye(length(D))+diagD*(V'*V)))*diagD*V'*Subset_labels);
 toc 
 
-diagD = diag(D);
-alpha = gam*(Labels_p - (V*inv((1/gam)*eye(length(D))+diagD*(V'*V)))*diagD*V'*Labels_p);
-
 b_p=0;
-[Ylabels_training, Zp] = simlssvm({X_p,Labels_p,'c',gam,sig2,'RBF_kernel','o'}, {alpha,b_p}, X_p);
-[Ylabels_validation, Zp] = simlssvm({X_p,Labels_p,'c',gam,sig2,'RBF_kernel','o'}, {alpha,b_p}, X_t);
+[Ylabels_training, Zp] = simlssvm({Subset,Subset_labels,'c',gam,sig2,'RBF_kernel','o'}, {alpha,b_p}, Subset);
+[Ylabels_validation, Zp] = simlssvm({Subset,Subset_labels,'c',gam,sig2,'RBF_kernel','o'}, {alpha,b_p}, X_t);
 
 
 num_correct      = sum(Ylabels_validation == Labels_t);
 num_incorrect    = length(Ylabels_validation)-num_correct;
 percentage_wrong = num_incorrect/length(Ylabels_validation)
 percentage_right = num_correct/length(Ylabels_validation)
-
-
-
-
-%% Search for good gamma 
-% for i=1:20
-%     gam = 0.5*10^(i-10);
-%     diagD = diag(D);
-%     alpha = gam*(Labels_p - (V*inv((1/gam)*eye(length(D))+diagD*V'*V))*diagD*V'*Labels_p);
-% 
-%     b_p=0;
-%     [Ylabels_training, Zp] = simlssvm({X_p,Labels_p,'c',gam,sig2,'RBF_kernel','original'}, {alpha,b_p}, X_p);
-%     [Ylabels_validation, Zp] = simlssvm({X_p,Labels_p,'c',gam,sig2,'RBF_kernel','original'}, {alpha,b_p}, X_t);
-% 
-% 
-%     num_correct      = sum(Ylabels_validation == Labels_t);
-%     num_incorrect    = length(Ylabels_validation)-num_correct;
-%     percentage_wrong = num_incorrect/length(Ylabels_validation);
-%     percentage_right = num_correct/length(Ylabels_validation);
-% 
-%     perf_gamma(:,i) = [percentage_right;gam]
-%     
-%     
-% end
-
-
